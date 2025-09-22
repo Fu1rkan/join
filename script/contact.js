18f08ed7a0879caa410f878b7b8c5966ee07760e
David Groß <d.g1r2o3s4s@gmail.com>
2025-09-22 17:32:33 +0200

    changed contacts.html, contact.js for template-testing and contact.css some styles

--- script/contact.js
+++ script/contact.js
@@ -7,3 +7,44 @@ function openOverlay() {
 function closeOverlay() {
     overlayRef.classList.add('d_none');
 }
+
+function showContact() {
+    let contactCardRef = document.getElementById('contact_card_am');
+    let name = document.getElementById('name_am').innerHTML;
+    let email = document.getElementById('email_am').innerHTML;
+
+    contactCardRef.classList.remove('contact-container-hoverclass')
+    contactCardRef.style.backgroundColor = "#2A3647"; 
+    let templateRef = document.getElementById('contact_template');
+    templateRef.innerHTML = getContactTemplate(name, email);
+}
+
+function getContactTemplate(name, email) {
+    return `<section class="contact-info-big">
+                        <img class="svg-img-big" src="./img/icons/test-am-icon-svg.svg" alt="">
+                        <div class="contact-info-big-name-edit-container">
+                            <p class="contact-info-big-name">${name}</p>
+                            <div class="contact-info-big-edit">
+                                <button class="contact-btn"><img class="contact-btn-img" src="./img/icons/edit.svg"
+                                        alt="Edit Icon">
+                                    <p class="contact-btn-txt">Edit</p>
+                                </button>
+                                <button class="contact-btn"><img class="contact-btn-img" src="./img/icons/delete.svg"
+                                        alt="Delete Icon">
+                                    <p class="contact-btn-txt">Delete</p>
+                                </button>
+                            </div>
+                        </div>
+                    </section>
+                    <p class="contact-inforamtion-txt">Contact Information</p>
+                    <section class="contact-informations">
+                        <div>
+                            <h5>Email</h5>
+                            <a href="mailto:antom@gmail.com">${email}</a>
+                        </div>
+                        <div>
+                            <h5>Phone</h5>
+                            <p class="contact-informations-phone-number">+49 1111 111 11 1</p>
+                        </div>
+                    </section>`
+}

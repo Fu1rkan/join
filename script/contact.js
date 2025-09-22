38500ecade43861c423a25a0a1ae2558fba6af96
David Groß <d.g1r2o3s4s@gmail.com>
2025-09-22 16:22:52 +0200

    changed contact.html added add-contact-overlay and changed navbar

---
+++ script/contact.js
@@ -0,0 +1,9 @@
+let overlayRef = document.getElementById("overlay");
+
+function openOverlay() {
+    overlayRef.classList.remove('d_none');
+}
+
+function closeOverlay() {
+    overlayRef.classList.add('d_none');
+}

cd2fa6f2e4d5dfd3fe3e6069874724d791a70b2c
CANOST-id <csinner31@gmail.com>
2025-09-26 15:07:30 +0200

    added functions for static header logo after animation

--- script/login.js
+++ script/login.js
@@ -1,4 +1,13 @@
+function init(){
+    setTimeout(renderJoinLogo, 990);
+}
+
 function renderSignUp() {
     document.body.innerHTML = '';
     document.body.innerHTML += signUpTemplate();
+    renderJoinLogo();
+}
+
+function renderJoinLogo() {
+    document.getElementById('main_header').innerHTML += joinLogoTemplate();
 }

a4c5fb1629d8d35f21e9b35e80617dcb4feeb4db
David Groß <d.g1r2o3s4s@gmail.com>
2025-10-05 17:52:17 +0200

    changed functionality (add-new-contact, delete-contact and edit-contact) and animations and highlighting

--- script/contact.js
+++ script/contact.js
@@ -13,10 +13,18 @@ function toggleContact(name, email, keys, index) {
     const templateRef = document.getElementById("contact_template");
     const contactRef = contacts.find(t => t.name === name && t.email === email);
     const isActive = contactCardRef.classList.contains("active-contact");
+    removeActiveClass();
+    highlightContactAndOpenContactTemplate(contactCardRef, templateRef, contactRef, isActive);
+}
+
+function removeActiveClass() {
     document.querySelectorAll("#contact_list li > div").forEach(el => {
         el.classList.remove("active-contact");
         el.classList.add("contact-container-hoverclass");
     });
+}
+
+function highlightContactAndOpenContactTemplate(contactCardRef, templateRef, contactRef, isActive) {
 
     if (!isActive) {
         contactCardRef.classList.add("active-contact");
@@ -28,15 +36,6 @@ function toggleContact(name, email, keys, index) {
         contactCardRef.classList.add("contact-container-hoverclass");
         templateRef.classList.add("d_none");
     }
-}
-
-
-function displayContact() {
-    let contactRef = document.getElementById('contact_card_am');
-    let contentAreaRef = document.getElementById('contact_template');
-
-    contactRef.style.display = "none";
-    contentAreaRef.style.display = "none";
 }
 
 function addNewContact() {
@@ -60,7 +59,6 @@ function saveChangedContact(name, email) {
     contactRef.name = contactName;
     contactRef.email = contactEmail;
     contactRef.phone = contactPhone;
-
     filterContacts();
     templateRef.classList.add('d_none')
 }
@@ -106,31 +104,17 @@ function renderContacts(groupedContacts) {
             listRef.innerHTML += getSmallContactTemplate(groupedContacts[keys[index]], i, keys[index].toLowerCase());
         }
     }
-
 }
 
 function renderContactList(keys) {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     const contactListRef = document.getElementById("contact_list");
-
-    contactListRef.innerHTML ="";
+    contactListRef.innerHTML = "";
     alphabet.forEach(letter => {
         const section = document.createElement("section");
         section.innerHTML = contactListTemplates(keys, letter);
-        contactListRef.appendChild(section);  
+        contactListRef.appendChild(section);
     });
-}
-
-function getSmallContactTemplate(array, index, keys) {
-    return `<li>
-                <div id="letter_${keys}_${index}" onclick="toggleContact('${array[index].name}', '${array[index].email}', '${keys}', '${index}')" class="contact-container contact-container-hoverclass">
-                    ${array[index].svg}
-                    <div class="contact-info">
-                        <h5 class="contact-name">${array[index].name}</h5>
-                        <a class="contact-e-mail" href="mailto:${array[index].email}">${array[index].email}</a>
-                    </div>
-                </div>
-            </li>`
 }
 
 function createNewContact() {
@@ -139,13 +123,111 @@ function createNewContact() {
     let createPhone = document.getElementById('create_phone').value;
 
     if (createName != "") {
+        let capitalizedName = generatecapitalizedName(createName);
+        let nameLetters = generateLetters(capitalizedName);
+        let fillColor = getRandomColor();
+        createObjectNewContact(capitalizedName, createEmail, createPhone, nameLetters, fillColor);
+        closeOverlay();
+        filterContacts();
+        findNewestContactAndHighlightIt(createName);
+    }
+}
+
+function generatecapitalizedName(createName) {
+    let nameArray = createName.split(" ");
+    let capitalizedArray = nameArray.map((word) => {
+        if (word != "") {
+            let firstLetter = word.charAt(0).toUpperCase();
+            let restName = word.slice(1);
+            return firstLetter + restName;
+        } else {
+            return word;
+        }
+    });
+    let resultFullName = capitalizedArray.join(" ");
+    return resultFullName;
+}
+
+function findNewestContactAndHighlightIt(createName) {
+    document.querySelectorAll('.active-contact').forEach(el => el.classList.remove('active-contact'));
+    // Find the index and key for the new contact
+    const groupedContacts = contacts.reduce((acc, contact) => {
+        const firstLetter = contact.name.trim().charAt(0).toUpperCase();
+        if (!acc[firstLetter]) acc[firstLetter] = [];
+        acc[firstLetter].push(contact);
+        return acc;
+    }, {});
+    const firstLetter = createName.trim().charAt(0).toUpperCase();
+    const index = groupedContacts[firstLetter].length - 1;
+    highlightNewestContact(firstLetter, index);
+}
+
+function highlightNewestContact(firstLetter, index) {
+    const contactElem = document.getElementById(`letter_${firstLetter.toLowerCase()}_${index}`);
+    if (contactElem) {
+        contactElem.classList.add('active-contact');
+        contactElem.classList.remove("contact-container-hoverclass");
+        contactElem.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+        });
+    }
+}
+
+function createObjectNewContact(createName, createEmail, createPhone, nameLetters, fillColor) {
+
+    if (createName != "") {
         let newContact = {
             "name": createName,
             "email": createEmail,
-            "phone": createPhone
+            "phone": createPhone,
+            "svg": createOutlinedCircleSVG(nameLetters, 42, fillColor, 12),
+            "svg_big": createOutlinedCircleSVG(nameLetters, 120, fillColor, 47)
         }
-
         contacts.push(newContact);
+        showCreatedContactTemplate(newContact);
     }
+}
+
+function generateLetters(capitolName) {
+    let createtFullName = capitolName;
+    let nameArray = createtFullName.split(" ");
+    let firstNameLetter = (nameArray[0]);
+    let lastNameletter = (nameArray[nameArray.length - 1]);
+    let firstLetters = firstNameLetter.charAt(0).concat(lastNameletter.charAt(0));
+    return firstLetters;
+}
+
+function getRandomColor() {
+    // Generate a random color with good contrast (avoid too light/dark colors)
+    let color;
+    do {
+        // Generate colors with each RGB channel between 64 and 192 for better visibility
+        const r = Math.floor(Math.random() * 128) + 64;
+        const g = Math.floor(Math.random() * 128) + 64;
+        const b = Math.floor(Math.random() * 128) + 64;
+        color = "#" + [r, g, b].map(x => x.toString(16).padStart(2, "0")).join("");
+    } while (
+        color.toLowerCase() === "#000000" ||
+        color.toLowerCase() === "#ffffff"
+    );
+    return color;
+}
+
+function showCreatedContactTemplate(newContact) {
+    const templateRef = document.getElementById("contact_template");
+    templateRef.classList.remove("d_none");
+    templateRef.innerHTML = getContactTemplate(newContact);
+    setTimeout(() => {
+        fadeInCreateMsg();
+    }, 800);
+}
+
+function deleteCurrentContact(name, email) {
+    const templateRef = document.getElementById("contact_template");
+    contacts.splice(contacts.findIndex(t => t.name == name && t.email == email),1);
     filterContacts();
+    templateRef.classList.add('d_none');
+
+    
 }

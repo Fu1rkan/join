71e663c94449e968e1be15d005a26efdeda89043
David Groß <d.g1r2o3s4s@gmail.com>
2025-10-24 19:08:03 +0200

    changed id-names and added fetching tasks

--- script/test_contacts.js
+++ script/test_contacts.js
@@ -1,58 +1,37 @@
-let contacts = [
-    {
-        "name": "Anton Mayer",
-        "email": "antonm@gmail.com",
-        "phone": "+49 1111 111 11 1",
-        "fillColor": "rgba(255, 122, 0, 1)",
-        "nameLetters" : "AM"
-    },
-    {
-        "name": "Anja Schulz",
-        "email": "schulz@gmail.com",
-        "phone": "+49 2222 222 22 2",
-        "fillColor": "rgba(147, 39, 255, 1)",
-        "nameLetters" : "AS"
-    },
-    {
-        "name": "Benedikt Ziegler",
-        "email": "benedikt@gmail.com",
-        "phone": "+49 3333 333 33 3",
-        "fillColor": "rgba(110, 82, 255, 1)",
-        "nameLetters" : "BZ"
-    },
-    {
-        "name": "David Eisenberg",
-        "email": "davidberg@gmail.com",
-        "phone" : "+49 4444 444 44 4",
-        "fillColor": "rgba(252, 113, 255, 1)",
-        "nameLetters" : "DE"
-    },
-    {
-        "name": "Eva Fischer",
-        "email": "eva@gmail.com",
-        "phone": "+49 5555 555 55 5",
-        "fillColor": "rgba(255, 187, 43, 1)",
-        "nameLetters" : "EF"
-    },
-    {
-        "name": "Emmanuel Mauer",
-        "email": "emmanuelma@gmail.com",
-        "phone": "+49 6666 666 66 6",
-        "fillColor": "rgba(31, 215, 193, 1)",
-        "nameLetters" : "EM"
-    },
-    {
-        "name": "Marcel Bauer",
-        "email": "bauer@gmail.com",
-        "phone": "+49 7777 777 77 7",
-        "fillColor": "rgba(31, 215, 77, 1)",
-        "nameLetters" : "MB"
-    },
-    {
-        "name": "Tatjana Wolf",
-        "email": "wolf@gmail.com",
-        "phone": "+49 8888 888 88 8",
-        "fillColor": "rgba(71, 31, 215, 1)",
-        "nameLetters" : "TW"
+let contacts = [];
+let testTasks = [];
+
+const BASE_URL = "https://testjoin-36a23-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function loadContacts() {
+    let user = "user/";
+    let userContacts = "contacts/";
+    let response = await fetch(BASE_URL + user + userContacts + ".json");
+    let responseToJson = await response.json();
+    pushUserContactsToArray(responseToJson);
+}
+
+function pushUserContactsToArray(responseToJson) {
+    for (let index = 0; index < responseToJson.length; index++) {
+        contacts.push(responseToJson[index]);
     }
-]
+    filterContacts();
+}
+
+
+//Tasks
+
+async function loadTasks() {
+    let user = "user/";
+    let userContacts = "tasks/";
+    let response = await fetch(BASE_URL + user + userContacts + ".json");
+    let responseToJson = await response.json();
+    pushUserTaskToArray(responseToJson);
+}
+
+function pushUserTaskToArray(responseToJson) {
+    for (let index = 0; index < responseToJson.length; index++) {
+        testTasks.push(responseToJson[index]);
+    }
+    console.log(testTasks);
+}

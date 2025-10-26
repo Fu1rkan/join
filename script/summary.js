e0c1914a341207962226b42988a1b2fea9dac70a
CANOST-id <csinner31@gmail.com>
2025-10-26 17:15:52 +0100

    edited overlay functionality incl. greeting rendering

--- script/summary.js
+++ script/summary.js
@@ -1,47 +1,61 @@
-/** Greeting changed by time*/
-    function getGreetingByHour(h) {
-      if (h >= 5 && h <= 11)   return "Good morning,";
-      if (h >= 12 && h <= 16)  return "Good afternoon,";
-      if (h >= 17 && h <= 21)  return "Good evening,";
-      return "Good night,";
-    }
-// Update greeting every minute
-    function updateGreeting() {
-      let now = new Date();
-      let hour = now.getHours();
-      document.getElementById("greeting-text").textContent = getGreetingByHour(hour);
-    }
+// Show and hide animated overlay for mobile devices when coming from login
+function checkAndShowOverlay() {
+    let urlParams = new URLSearchParams(window.location.search);
+    let showOverlay = urlParams.get('showOverlay');
+    let loginType = urlParams.get('loginType');
 
-    updateGreeting();
-    setInterval(updateGreeting, 60);
+    if (showOverlay === 'true' && window.innerWidth < 900) {
+        let overlay = document.getElementById('animated_overlay_id');
+            if (loginType === 'guest') {
+                overlay.innerHTML = summaryGuestOverlayTemplate();
+            } else {
+                overlay.innerHTML = summaryOverlayTemplate();
+            }
+            setTimeout(() => {
+                updateGreeting();
+            }, 100);
+            overlay.classList.add('show');
+            setTimeout(() => {
+                overlay.classList.remove('show');
+            }, 3000);
+    }
+    let newUrl = window.location.pathname;
+    window.history.replaceState(null, '', newUrl);
+}
+
+checkAndShowOverlay();/** Greeting changed by time*/
+function getGreetingByHour(h) {
+    if (h >= 5 && h <= 11) return "Good morning,";
+    if (h >= 12 && h <= 16) return "Good afternoon,";
+    if (h >= 17 && h <= 21) return "Good evening,";
+    return "Good night,";
+}
+// Update greeting every minute
+function updateGreeting() {
+    let now = new Date();
+    let hour = now.getHours();
+    document.getElementById("greeting-text").textContent = getGreetingByHour(hour);
+}
+
+updateGreeting();
+setInterval(updateGreeting, 60);
+
 
 
 // Menu Toggle function
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let profileButton = document.getElementById('profile-toggle');
     let profileMenu = document.getElementById('profile-menu');
-    
+
     // Toggle Menu click action
-    profileButton.addEventListener('click', function() {
+    profileButton.addEventListener('click', function () {
         profileMenu.classList.toggle('show');
     });
-    
+
     // Menu close when clicking outside
-    document.addEventListener('click', function(event) {
+    document.addEventListener('click', function (event) {
         if (!profileMenu.contains(event.target) && !profileButton.contains(event.target)) {
             profileMenu.classList.remove('show');
         }
     });
 });
-
-function renderLogInUserAnimation() {
-    setTimeout(() => {
-    }, 3000);
-    closeLogInUserAnimation();
-}
-
-// Close animation overlay after login
-function closeLogInUserAnimation() {
-    let overlayRef = document.getElementById('animated_overlay_id');
-    overlayRef.classList.remove('animated-overlay');
-}

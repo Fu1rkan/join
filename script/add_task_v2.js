4cd03f39b1651114760b99aebd5e5309f81020ea
David Groß <d.g1r2o3s4s@gmail.com>
2025-10-27 17:32:13 +0100

    worked on assigned-to contactlist

--- script/add_task_v2.js
+++ script/add_task_v2.js
@@ -1,7 +1,7 @@
 let priorityTaskActive = "medium";
 
 function activatePriority(para = "medium") {
-    PriorityTaskActive ="";
+    PriorityTaskActive = "";
     resetPriorityButtonHighlight();
     let buttonRef = document.getElementById(`add_task_priority_${para}`);
     let svgRef = document.getElementById(`add_task_priority_${para}_svg`);
@@ -11,8 +11,8 @@ function activatePriority(para = "medium") {
         highlightPriorityButton(para, buttonRef, svgRef);
     } else {
         highlightPriorityButton(para, buttonRef, svgRef);
-    }  
-    priorityTaskActive = para;      
+    }
+    priorityTaskActive = para;
 }
 
 function resetPriorityButtonHighlight() {
@@ -29,4 +29,15 @@ function highlightPriorityButton(para, buttonRef, svgRef) {
     buttonRef.classList.remove('add-task-priority-btns-hover-class');
     buttonRef.classList.add(`add-task-priority-btn-${para}-active`);
     svgRef.classList.add(`add_task_priority_active_svg`);
+}
+
+
+function selectContact(contact) {
+    let contactRef = document.getElementById(contact);
+    let svgUncheckedRef = document.getElementById(`${contact}_unchecked`);
+    let svgCheckedRef = document.getElementById(`${contact}_checked`);
+    contactRef.classList.remove('add-task-form-assigned-to-dropdown-contacts-default-hover-class')
+    contactRef.classList.add('add-task-form-assigned-to-dropdown-contacts-checked')
+    svgUncheckedRef.classList.add('d_none');
+    svgCheckedRef.classList.remove('d_none');
 }

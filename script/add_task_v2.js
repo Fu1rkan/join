fc2c7f2c915693752ccd540a17c0be2266a757a7
David Groß <d.g1r2o3s4s@gmail.com>
2025-10-27 14:21:33 +0100

    added js for highlight priority-btns

---
+++ script/add_task_v2.js
@@ -0,0 +1,30 @@
+
+
+function activatePriority(para) {
+    resetPriorityButtonHighlight();
+    let buttonRef = document.getElementById(`add_task_priority_${para}`);
+    let svgRef = document.getElementById(`add_task_priority_${para}_svg`);
+    if (para == 'urgent') {
+        highlightPriorityButton(para, buttonRef, svgRef);
+    } else if (para == 'medium') {
+        highlightPriorityButton(para, buttonRef, svgRef);
+    } else {
+        highlightPriorityButton(para, buttonRef, svgRef);
+    }
+}
+
+function resetPriorityButtonHighlight() {
+    document.querySelectorAll('.add-task-priority-btns-container button').forEach(btn => {
+        btn.classList.remove('add-task-priority-btn-urgent-active', 'add-task-priority-btn-medium-active', 'add-task-priority-btn-low-active');
+        btn.classList.add('add-task-priority-btns-hover-class');
+    });
+    document.querySelectorAll('.add-task-priority-btns-container button svg').forEach(svg => {
+        svg.classList.remove('add_task_priority_active_svg');
+    })
+}
+
+function highlightPriorityButton(para, buttonRef, svgRef) {
+    buttonRef.classList.remove('add-task-priority-btns-hover-class');
+    buttonRef.classList.add(`add-task-priority-btn-${para}-active`);
+    svgRef.classList.add(`add_task_priority_active_svg`);
+}

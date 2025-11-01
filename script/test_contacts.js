f4390fbd14ff63ba03e0c569a6b4f1639cd0457a
CANOST-id <csinner31@gmail.com>
2025-11-01 19:14:39 +0100

    deleted comment code

--- script/test_contacts.js
+++ script/test_contacts.js
@@ -51,7 +51,6 @@ function pushUserContactsToArray(responseToJson) {
   
 }
 
-
 //Tasks
 
 async function loadTasks(userPath) {
@@ -71,54 +70,6 @@ function pushUserTaskToArray(responseToJson) {
     }
   }
 }
-
-
-
-
-//Kopie von Svens Code zum Task erstellen
-
-// function checkNPost(formId, dateId) {
-//   let title = document.getElementById("title");
-//   let description = document.getElementById("description").value;
-//   let dueDate = document.getElementById(dateId);
-//   let priority = document.getElementById("priority").value;
-//   let category = document.getElementById("category_input");
-
-//   if (title.value.length < 1 || dueDate.value.length < 1 || category.value.length < 1) {
-//     requiredNotice(title, "title");
-//     requiredNotice(dueDate, "due_date");
-//     requiredNotice(category, "category");
-
-//   } else {
-//     postTask("", {
-//     "name": title.value,
-//     "description": description,
-//     "date": dueDate.value,
-//     "priority": priority,
-//     "assigned_to": "placeholder",
-//     "category": category.value,
-//     "subtask": ["text1", "text2", "text3"],
-//       /* add the other inputs */
-
-//     let newestTask = {
-//       "id" : taskList.length,
-//       "name": title.value,
-//       "description": description,
-//       "date": dueDate.value,
-//       "priority": priority,
-//       "participants": false,
-//       "type": category.value,
-//       "category": "to-do",
-//       "subtasks": false,
-//     }
-
-//     taskList.push(newestTask);
-//     clearFormAddTask(formId)
-
-//     postTask("user/tasks/", taskList);
-//     init();
-//   };
-// }
 
 // post tasks to database
 async function postTask(path, data = {}) {    // "user/tasks/", testTasks

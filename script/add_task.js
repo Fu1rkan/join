56ba2b28329706f67d1094732208f6281eb79445
Wognod <sven.degen1998@gmail.com>
2025-11-04 21:06:08 +0100

    Call renderCurrentAssignedTo in clearForm
    Added a call to renderCurrentAssignedTo() in the clearForm() function to ensure the assigned users are updated when the form is cleared.

--- script/add_task.js
+++ script/add_task.js
@@ -333,6 +333,7 @@ function clearForm() {
     activatePriority();
     resetGlobalVariables();
     inputFieldAddFormCalenderSvgRef.classList.remove('d_none');
+    renderCurrentAssignedTo()
     renderCurrentCreatedSubtasks()
 }
 

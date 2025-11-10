4bb70f4b011a6c3835a401969a95d3e32c77d3f7
CANOST-id <csinner31@gmail.com>
2025-11-10 20:15:44 +0100

    fixed input issues

--- script/template.js
+++ script/template.js
@@ -114,7 +114,7 @@ function signUpTemplate() {
                     <li>
                         <form action="username" class="username_input" id="username_input_id">
                             <label>
-                                <input type="text" id="username" name="username" placeholder="Username">
+                                <input type="text" id="username" name="username" placeholder="Username" autocomplete="new-username">
                                 <div>
                                 <svg class="user_icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                     <path
@@ -129,7 +129,7 @@ function signUpTemplate() {
                     <li>
                         <form action="email" class="email_input" id="email_input_id">
                             <label for="email">
-                                <input type="Email" id="email" name="Email" placeholder="Email">
+                                <input type="Email" id="email" name="Email" placeholder="Email" autocomplete="new-email">
                                 <div>
                                 <svg class="email_icon" xmlns="http://www.w3.org/2000/svg" width="20" height="16" viewBox="0 0 20 16" fill="none">
                                     <path
@@ -144,7 +144,7 @@ function signUpTemplate() {
                     <li>
                         <form action="password_input" class="password_input" id="password_input_id">
                             <label for="password">
-                                <input class="password_input" type="password" id="password" name="password" placeholder="Password">
+                                <input class="password_input" type="password" id="password" name="password" placeholder="Password" autocomplete="new-password">
                                 <div id="toggle_password_visibility_button">
                                     <svg class="password_lock_icon" xmlns="http://www.w3.org/2000/svg" width="16" height="21"
                                         viewBox="0 0 17 22" fill="none">
@@ -160,7 +160,7 @@ function signUpTemplate() {
                     <li>
                         <form action="password_input" class="password_input" id="password_repeat_input_id">
                             <label for="password_repeat">
-                                <input class="password_input" type="password" id="password_repeat" name="password_repeat" placeholder="Confirm Password">
+                                <input class="password_input" type="password" id="password_repeat" name="password_repeat" placeholder="Confirm Password" autocomplete="new-password">
                                 <div id="toggle_password_repeat_button">
                                     <svg class="password_lock_icon" xmlns="http://www.w3.org/2000/svg" width="16" height="21"
                                         viewBox="0 0 17 22" fill="none">
